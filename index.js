1ad34829590b10532d54779bc4a1721541628f61
Muhammad Darwis <75767311+darwisabdrauf@users.noreply.github.com>
2024-06-21 15:19:59 +0800

    feat: add some interactivity & major refactor
    - added interactivity for each link and button for the website.
    - used Tailwind CSS CDN to customize the theme (font) instead of using traditional CSS stylesheet
    - refactor some of the html syntax using semantic attribute
    - refactor code for the overall layout of the website to make it looks clean on every screen sizes

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
+const hamburgerBtn = document.getElementById("btn-hamburger")
+const closeBtn = document.getElementById("btn-close")
+const navLinkMain = document.getElementById("nav-main")
+const navLinkSecondary = document.getElementById("nav-secondary")
 const disclaimer = document.getElementById("disclaimer")
-const link = document.querySelectorAll("a")
-const hamburgerBtn = document.getElementById("hamburger")
+const btnFAQ = document.querySelectorAll(".btn-faq")
+const navLink = document.querySelectorAll(".anchor-link")
 
 // EVENT LISTENER
-disclaimer.addEventListener("click", () => {
-    alert("Penafian (Disclaimer): Laman web ini bukan laman web rasmi PADU. Laman web ini hanyalah sebuah laman web demo dan ia dibina sebagai salah satu projek peribadi saya.")
-})
+hamburgerBtn.addEventListener("click", toggleNavigation)
+closeBtn.addEventListener("click", toggleNavigation)
+navLinkMain.addEventListener("click", toggleNavigation)
 
-link.forEach(link => {
+navLink.forEach(link => {
     link.addEventListener("click", () => {
         alert("Kebiasaannya, pautan ini berfungsi. Tetapi untuk tujuan demonstrasi, pautan ini tidak akan bawa anda ke destinasi tertentu.")
     })
 })
 
-hamburgerBtn.addEventListener("click", () => {
-    console.log("It's working")
+disclaimer.addEventListener("click", () => {
+    alert(`Penafian (Disclaimer): Laman web ini bukanlah laman web rasmi PADU. Laman web ini hanyalah sebuah laman web demo dan ia dibina sebagai salah satu projek peribadi saya.
+
+Saya hanya memfokuskan kepada susun atur (layout) pada laman ini dan ia tidak sama sekali meminta apa-apa maklumat sensitif daripada anda.`)
 })
+
+btnFAQ.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        const targetId = e.currentTarget.getAttribute("data-target")
+        const targetEl = document.getElementById(targetId)
+
+        targetEl.classList.toggle("hidden")
+
+        // change icon graphic
+        const icon = e.currentTarget.querySelector("i")
+        icon.classList.toggle("fa-chevron-down")
+        icon.classList.toggle("fa-chevron-up")
+    })
+})
+
+
+// FUNCTION
+function toggleNavigation() {
+    navLinkMain.classList.toggle("hidden")
+    navLinkSecondary.classList.toggle("hidden")
+    hamburgerBtn.classList.toggle("hidden")
+    closeBtn.classList.toggle("hidden")
+}
+
